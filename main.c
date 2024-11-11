d6b1faf195a5b13bfb3895d334a221b15878b841
IureDiniz <iuredinizsoares@gmail.com>
2024-11-11 15:41:36 -0300

    teste 1

--- main.c
+++ main.c
@@ -1,10 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
 
-    //insira aqui seu código
-    printf("Nao implementado ainda...");
+int main(){
+    int num_cate, aux;
+    scanf("%d", &num_cate);
+    if(num_cate != 0){
+        int *p = (int*) malloc(num_cate * sizeof(int*));
+    
+        for(int i = 0; i < num_cate; i++){
+            scanf("%d", &aux);
+         p[i] = aux;
+        }
+
+        for(int i = 0; i < num_cate; i++){
+            printf("%d ", p[i]);
+            for(int j = 0; j < p[i]; j++){
+                printf("*");
+            }
+            printf("\n");
+        }
+
+        free(p);
+
+    }else{
+        printf("vazio");
+    }
+
     
     return 0;
 }
